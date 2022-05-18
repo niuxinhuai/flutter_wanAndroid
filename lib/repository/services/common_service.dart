ed2372fc5c842f6f2316935a6228e56040aaa0e2
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-05-18 14:19:02 +0800

    add home banner article service

--- lib/repository/services/common_service.dart
+++ lib/repository/services/common_service.dart
@@ -1,1 +1,16 @@
-class CommonService {}
+import 'package:flutter_wanandroid/constants/uri.dart';
+import 'package:flutter_wanandroid/helper/service_helper.dart';
+import 'package:flutter_wanandroid/sections/home/models/article/article.dart';
+import 'package:flutter_wanandroid/sections/home/models/banner/banner.dart';
+
+class CommonService {
+  ///首页banner
+  static Future<HomeBannerWrap> getHomeBanner() =>
+      ServiceHelper.get(Uri.home_banner)
+          .then((json) => HomeBannerWrap.fromJson(json));
+
+  ///首页文章
+  static Future<HomeArticleWrap> getHomeArticle({int page = 0}) =>
+      ServiceHelper.get(Uri.home_article(page))
+          .then((json) => HomeArticleWrap.fromJson(json));
+}
