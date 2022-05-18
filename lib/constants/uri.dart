6ee468da6bff0557cbf1b03fda6c5e2f89161db9
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-05-18 14:17:59 +0800

    add home banner article uri

--- lib/constants/uri.dart
+++ lib/constants/uri.dart
@@ -1,4 +1,10 @@
 class Uri {
   ///base
   static const String baseUri = "https://www.wanandroid.com/";
+
+  ///首页banner
+  static const String home_banner = "banner/json";
+
+  ///首页文章列表
+  static String home_article(int page) => "article/list/$page/json";
 }
