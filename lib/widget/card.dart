8852dd062e2d81ba46f27b9a300687ece7f13f1d
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-05-19 17:24:25 +0800

    widget

---
+++ lib/widget/card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class GpCard extends StatelessWidget {
+  final Widget child;
+  final double? width;
+  final double? height;
+  final EdgeInsets? padding;
+  final EdgeInsets? margin;
+
+  const GpCard(
+      {required this.child,
+      this.padding,
+      this.margin,
+      this.width,
+      this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surface,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black.withOpacity(0.07),
+                  offset: Offset(0, 2),
+                  blurRadius: 6.0)
+            ],
+            borderRadius: BorderRadius.circular(6)),
+        padding: padding ?? EdgeInsets.zero,
+        margin: margin ?? EdgeInsets.zero,
+        child: child);
+  }
+}
