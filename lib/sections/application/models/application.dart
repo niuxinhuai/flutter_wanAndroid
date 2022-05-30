18021af1adf8763a1a1ed578a6a5bceaf733bfdd
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-05-30 16:19:44 +0800

    application

--- lib/sections/application/models/application.dart
+++ lib/sections/application/models/application.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_wanandroid/res/colors.dart';
 
 class ApplicationModule {
-  static const List<String> tabTitles = ['首页', '体系', '公众号', '我的'];
+  static const List<String> tabTitles = ['首页', '体系', '公众号', '导航', '项目'];
   static const List<IconData> tabIcons = [
     Icons.home,
     Icons.message,
     Icons.account_balance_wallet,
+    Icons.navigation,
     Icons.settings
   ];
 
--- lib/sections/application/page/state.dart
+++ lib/sections/application/page/state.dart
@@ -2,6 +2,8 @@ import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart' as P;
 import 'package:flutter_wanandroid/sections/home/page/home/page.dart';
 import 'package:flutter_wanandroid/sections/knowledge/page/knowledge/page.dart';
+import 'package:flutter_wanandroid/sections/navigation/page/navigation/page.dart';
+import 'package:flutter_wanandroid/sections/wechat/page/wechat/page.dart';
 import 'package:flutter_wanandroid/widget/segment.dart';
 
 class ApplicationState implements Cloneable<ApplicationState> {
@@ -27,16 +29,13 @@ ApplicationState initState(Map<String, dynamic>? args) {
     childWidget: KnowledgePage().buildPage({}),
   ));
   children.add(KeepAliveWidget(
-    childWidget: P.Container(
-      alignment: P.Alignment.center,
-      child: P.Text('公众号'),
-    ),
+    childWidget: WeChatArticlePage().buildPage({}),
   ));
   children.add(KeepAliveWidget(
-    childWidget: P.Container(
-      alignment: P.Alignment.center,
-      child: P.Text('我的'),
-    ),
+    childWidget: NavigationArticlePage().buildPage({}),
+  ));
+  children.add(KeepAliveWidget(
+    childWidget: P.Text('项目'),
   ));
 
   return ApplicationState()..children = children;
