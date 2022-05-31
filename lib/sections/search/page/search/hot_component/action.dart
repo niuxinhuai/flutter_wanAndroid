024a80d63ecf29fa0ee628a46bf690b0b48fc2fd
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-05-31 16:23:39 +0800

    other

--- lib/sections/search/page/search/hot_component/action.dart
+++ lib/sections/search/page/search/hot_component/action.dart
@@ -1,7 +1,6 @@
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter_wanandroid/sections/search/models/hot_search.dart';
 
-//TODO replace with your own action
 enum SearchHotAction { action, onTapItem }
 
 class SearchHotActionCreator {
