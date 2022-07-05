78d5adbe7d3295d98c20b199979b772b1fb10909
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-07-05 11:12:20 +0800

    weatherkey

--- lib/constants/key.dart
+++ lib/constants/key.dart
@@ -2,4 +2,6 @@ class ConstantsKey {
   static final String encrypterKey =
       "my 32 length key................"; //qwertyuiop1234567lkjhgf
   static final String encrypterIV = "0123456789ABCDEF";
+
+  static final String weatherToken = "oU6JnqAWhL9mMHks";
 }
