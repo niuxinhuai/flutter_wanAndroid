8480202131350c59d720230bf369e19ff32ffe29
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-07-05 11:16:24 +0800

    home drawer

---
+++ lib/sections/home/page/drawer/effect.dart
@@ -0,0 +1,51 @@
+import 'package:fish_redux/fish_redux.dart';
+import 'package:flutter/material.dart' as P;
+import 'package:flutter_wanandroid/helper/router.dart';
+import 'package:flutter_wanandroid/helper/router_helper.dart';
+import 'package:flutter_wanandroid/helper/user.dart';
+import 'package:flutter_wanandroid/repository/database/common_database.dart';
+import 'package:flutter_wanandroid/sections/login/models/login.dart';
+import 'package:flutter_wanandroid/widget/toast.dart';
+import 'action.dart';
+import 'state.dart';
+import 'package:flutter_wanandroid/helper/flutter_helper.dart';
+
+Effect<HomeDrawerState>? buildEffect() {
+  return combineEffects(<Object, Effect<HomeDrawerState>>{
+    Lifecycle.initState: _initState,
+    HomeDrawerAction.action: _onAction,
+    HomeDrawerAction.onTapSegment: _onTapSegment,
+  });
+}
+
+void _onAction(Action action, Context<HomeDrawerState> ctx) {}
+
+void _initState(Action action, Context<HomeDrawerState> ctx) async {
+  ctx.state.isLogin = UserDefault.isLogin();
+  ctx.state.titles.last = ctx.state.isLogin ? "退出登录" : "登录";
+}
+
+void _onTapSegment(Action action, Context<HomeDrawerState> ctx) async {
+  String value = action.payload;
+  P.Navigator.pop(ctx.context);
+  if (value == "我的收藏") {
+    if (!ctx.state.isLogin) {
+      Toast.toast(ctx.context, "登录后可查看我的收藏");
+    } else {
+      ///去我的收藏
+      ARouter.open(ctx.context, RouterKeys.collect);
+    }
+  } else if (value == "退出登录") {
+    LoginWrap? wrap = await UserDefault.logout();
+    if (wrap != null && wrap.errorCode == 0) {
+      UserDefault.clear();
+      await CommonDb.deleteUserSearchHistory();
+      await CommonDb.deleteUserCollectHistory();
+      eventBus.fire(LoginWrap());
+    } else {
+      Toast.toast(ctx.context, "退出失败，请重试");
+    }
+  } else if (value == "登录") {
+    ARouter.open(ctx.context, RouterKeys.login);
+  }
+}
---
+++ lib/sections/home/page/drawer/state.dart
@@ -0,0 +1,33 @@
+import 'package:fish_redux/fish_redux.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeDrawerState implements Cloneable<HomeDrawerState> {
+  bool isLogin = true;
+  List<String> titles = [];
+  List<IconData> icons = [];
+
+  @override
+  HomeDrawerState clone() {
+    return HomeDrawerState()
+      ..isLogin = isLogin
+      ..titles = titles
+      ..icons = icons;
+  }
+}
+
+HomeDrawerState initState(Map<String, dynamic>? args) {
+  List<String> titles = ["我的收藏", "天气", "TODO", "设置", "关于", "登录"];
+  List<IconData> icons = [
+    Icons.favorite_border,
+    Icons.cloud,
+    Icons.message,
+    Icons.settings,
+    Icons.watch_later,
+    Icons.logout
+  ];
+
+  return HomeDrawerState()
+    ..titles = titles
+    ..icons = icons;
+}
