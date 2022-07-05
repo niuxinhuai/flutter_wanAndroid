da4f04a34fd14ce7d1e2bff4f53b2ac27582992e
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-07-05 11:12:42 +0800

    eventBus

--- lib/helper/flutter_helper.dart
+++ lib/helper/flutter_helper.dart
@@ -1,4 +1,7 @@
+import 'package:event_bus/event_bus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_boost/flutter_boost.dart';
+
+EventBus eventBus = EventBus();
 
 class FlutterBindings extends WidgetsFlutterBinding with BoostFlutterBinding {}
