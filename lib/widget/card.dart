80f6153418c69c7cdf15cff6321fadb0364c20fa
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-07-15 11:47:30 +0800

    widget

--- lib/widget/card.dart
+++ lib/widget/card.dart
@@ -6,28 +6,31 @@ class GpCard extends StatelessWidget {
   final double? height;
   final EdgeInsets? padding;
   final EdgeInsets? margin;
+  final BoxDecoration? decoration;
 
   const GpCard(
       {required this.child,
       this.padding,
       this.margin,
       this.width,
-      this.height});
+      this.height,
+      this.decoration});
 
   @override
   Widget build(BuildContext context) {
     return Container(
         width: width,
         height: height,
-        decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.surface,
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black.withOpacity(0.07),
-                  offset: Offset(0, 2),
-                  blurRadius: 6.0)
-            ],
-            borderRadius: BorderRadius.circular(6)),
+        decoration: decoration ??
+            BoxDecoration(
+                color: Theme.of(context).colorScheme.surface,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black.withOpacity(0.07),
+                      offset: Offset(0, 2),
+                      blurRadius: 6.0)
+                ],
+                borderRadius: BorderRadius.circular(6)),
         padding: padding ?? EdgeInsets.zero,
         margin: margin ?? EdgeInsets.zero,
         child: child);
