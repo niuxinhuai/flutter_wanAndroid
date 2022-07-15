d9601f6d7963cbf35bd39b24416dab12cf71b60c
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-07-15 11:44:01 +0800

    Iconfont

---
+++ lib/res/iconfont.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class IconF {
+  IconF._();
+
+  static const _font_name = "iconfont";
+
+  static const IconData yifu = const IconData(0xe666, fontFamily: _font_name);
+
+  static const IconData gaoman = const IconData(0xe60f, fontFamily: _font_name);
+
+  static const IconData qiche = const IconData(0xec6d, fontFamily: _font_name);
+
+  static const IconData taiyang =
+      const IconData(0xe611, fontFamily: _font_name);
+}
